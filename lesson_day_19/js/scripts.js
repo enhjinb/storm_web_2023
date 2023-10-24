6c9da459da46820acb4f6c07f7b73009ada018a9
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-10-24 10:38:49 +0800

    lesson day 19

---
+++ lesson_day_19/js/scripts.js
@@ -0,0 +1,44 @@
+console.log ('Lesson Day 19 -JS Control structures exercises')
+let too1 = 10;
+let too2= 20;
+let too3 = 30;
+
+let ner1 = 'Bold';
+let ner2 = 'Tulga';
+let ner3 = 'Naraa';
+
+let unen = true;
+let hudal = false;
+
+
+let isMongolian = true;
+let hasMongolianPassword = false;
+
+if (isMongolian) {
+    console.log('You are Mongolian.');
+}
+
+// if else statement
+if (isMongolian) {
+    console.log('You are Mongolian.');
+} else if (isMongolian && hasMongolianPassword){
+    console.log('You are Mongolian.');
+} else {
+    console.log('You are not Mongolian.');
+}
+
+// Loops
+// for loop
+for ( let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+// while loop
+let i = 0;
+while (i < 10) {
+    console.log(i);
+    i++;
+}
+
+
+
