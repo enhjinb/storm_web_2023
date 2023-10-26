69c92cf098f760f2c3415edd229b5499aa7b3853
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-10-26 13:04:39 +0800

    lesson day 20

---
+++ lesson_day_20/js/array_script.js
@@ -0,0 +1,15 @@
+console.log('Lesson Day 20 - JS Arrays');
+var myNumbers = [5, 8, 103, 32, 7]; // array of numbers
+console.log(myNumbers); // [5, 8, 103, 32, 7]
+
+var shoppingList = ['milk', 'eggs', 'bread', 'cheese', 'butter',]
+console.log(shoppingList); // [ 5, 7, 'milk', 'eggs', true, false ]
+
+// accressing array elements 
+
+console.log(myNumbers [2]); //103
+
+// change array elements
+console.log('before change shoppingList:', shoppingList)
+shopppingList[3] = 'meat';
+console.log('after change shoppingList:', shoppingList)
---
+++ lesson_day_20/js/scripts.js
@@ -0,0 +1,7 @@
+var para = document.getElementById
+console.log(para);
+para.innerHTML = "This is an altered paragraph";
+
+var myName = prompt('What is your name?');
+var myNameParagraph = document.getElementById (my_name);
+myNameParagraph.innnerHTML = myName;
