dde6777948b2ea1c8c36d29676fbec5503b51378
OtgonmunkhG <g.otgonmunkh@gmail.com>
2023-11-04 10:57:34 +0800

    lesson day 24

---
+++ lesson_day_24/js/evenet.js
@@ -0,0 +1,1 @@
+let ulElement = document.createElement
---
+++ lesson_day_24/js/script.js
@@ -0,0 +1,14 @@
+console.log('Lesson Day 24 - JS Function, JS DOM keyboard event');
+
+function myFunction() {
+    console.log('it is my function');
+}
+myFunction();
+function addTwo (number){
+    let x = number + 2;
+    return x;
+}
+console.log(addTwo(5));
+console.log(addTwo(11));
+console.log(addTwo(45));
+
